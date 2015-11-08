2e5e09b10d8cdd51d6baee5cb2ec3dde2d11fe2a
Jonathan Ullom <jonu001@yahoo.com>
2015-11-07 21:54:11 -0500

    removed old petfinder api calls

--- application/controllers/Adoptable.php
+++ application/controllers/Adoptable.php
@@ -14,24 +14,10 @@ class Adoptable extends CI_Controller {
 
 	function getPets()
 	{
-		//$petfinder_id = '32288544'; // wonka
 		$petfinder_id = 'PA660'; // fur kid rescue
 		$key = $this->config->item('key', 'petfinder_api');
 		$secret = $this->config->item('secret', 'petfinder_api');
 		$token_sig = md5($secret . 'format=json&key=' . $key);
-
-		//xml
-		//$petfinder_xml = simplexml_load_string(file_get_contents('http://api.petfinder.com/auth.getToken?key=' . $key . '&sig=' . $token_sig));
-		//$token = $petfinder_xml->auth->token;
-		//$wonka = simplexml_load_string(file_get_contents('http://api.petfinder.com/pet.get?key=' . $key . '&id=32288544&token=' . $token . '&sig=' . $request_sig));
-		
-		//JSON with file_get_contents
-		/*
-		$token_json = json_decode(file_get_contents('http://api.petfinder.com/auth.getToken?format=json&key=' . $key . '&sig=' . $token_sig));
-		$token = $token_json->{'petfinder'}->{'auth'}->{'token'}->{'$t'};
-		$request_sig = md5($secret . 'format=json&key=' . $key . '&id=' . $petfinder_id . '&token=' . $token);
-		$furkid = json_decode(file_get_contents('http://api.petfinder.com/shelter.getPets?format=json&key=' . $key . '&id=' . $petfinder_id . '&token=' . $token . '&sig=' . $request_sig));
-		*/
 
 		$token_json = json_decode($this->curl_download('http://api.petfinder.com/auth.getToken?format=json&key=' . $key . '&sig=' . $token_sig));
 		$token = $token_json->{'petfinder'}->{'auth'}->{'token'}->{'$t'};
